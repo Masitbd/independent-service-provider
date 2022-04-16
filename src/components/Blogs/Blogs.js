88caca300d1b4da7b5381f388af658a4c51a6064
Murad Sarker <sarker24@gmail.com>
2022-04-16 18:53:28 +0200

    Add route in app file

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Blogs.css";
+
+const Blogs = () => {
+  return (
+    <div>
+      <h2>blogs page</h2>
+    </div>
+  );
+};
+
+export default Blogs;
+<h2>blogs page</h2>;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Foorer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>footer page</h2>
+    </div>
+  );
+};
+
+export default Footer;
+<h2>footer page</h2>;
