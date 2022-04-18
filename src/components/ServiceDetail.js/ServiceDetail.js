d362a891404e2c903df72c674449eead56cc4fcd
Murad Sarker <sarker24@gmail.com>
2022-04-18 04:35:06 +0200

    Add contact form

--- src/components/ServiceDetail.js/ServiceDetail.js
+++ src/components/ServiceDetail.js/ServiceDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
@@ -6,7 +7,76 @@ const ServiceDetail = () => {
   console.log(serviceid);
   return (
     <div>
-      <h2>service detaile page</h2>
+      <Container>
+        <h2 className="text-center text-success mt-3">MAKE AN APPOINTMENT</h2>
+        <form className="w-50 mx-auto">
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="First Name"
+            id=""
+          />
+          <br />
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="Last Name"
+            id=""
+          />
+          <br />
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="Subject"
+            id=""
+          />
+          <br />
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="Date"
+            id=""
+          />
+          <br />
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="Your Email"
+            id=""
+          />
+          <br />
+          <input
+            className="w-100 mt-2"
+            style={{ height: "40px" }}
+            type="text"
+            name=""
+            placeholder="Your Phone"
+            id=""
+          />
+          <br />
+          <textarea
+            className="w-100 mt-2"
+            style={{ height: "200px" }}
+            name=""
+            id=""
+            placeholder="Write Message"
+            cols="30"
+            rows="5"
+          ></textarea>
+          <br />
+          <button className="btn btn-primary px-4">Send Message</button>
+        </form>
+      </Container>
     </div>
   );
 };
